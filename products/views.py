0e8008d8c905716413c972e4360b692341aadf60
Angel Diaz <warydiaz@hotmail.com>
2023-02-03 14:37:42 +0100

    Adding model and BD

--- products/views.py
+++ products/views.py
@@ -2,7 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-
-
 def index(request):
     return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('New Products')
+
